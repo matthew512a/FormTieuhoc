8d175201874a140fb4dc174c0be9fd5c6c19fc41
matthew512a <kkien20030512@gmail.com>
2024-05-18 22:18:57 +0700

    đã làm xong form cộng trừ nhân chia 3 số

---
+++ PhepToan3So.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace FormDangNhap
+{
+    partial class PhepToan3So
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhepToan3So));
+            label1 = new Label();
+            lblSo1 = new Label();
+            lblToanTu1 = new Label();
+            lblSo2 = new Label();
+            lblToanTu2 = new Label();
+            lblSo3 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtDoan = new TextBox();
+            lblKq = new Label();
+            btnXem = new Button();
+            btnTiepTuc = new Button();
+            btnXoa = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            btnDauTru = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            btn0 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(1109, 99);
+            label1.TabIndex = 0;
+            label1.Text = "PHÉP TOÁN CƠ BẢN VỚI 3 SỐ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblSo1
+            // 
+            lblSo1.BackColor = Color.FromArgb(255, 224, 192);
+            lblSo1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSo1.Location = new Point(129, 152);
+            lblSo1.Name = "lblSo1";
+            lblSo1.Size = new Size(116, 109);
+            lblSo1.TabIndex = 1;
+            lblSo1.Text = "0";
+            lblSo1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblToanTu1
+            // 
+            lblToanTu1.BackColor = Color.FromArgb(255, 224, 192);
+            lblToanTu1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblToanTu1.Location = new Point(311, 152);
+            lblToanTu1.Name = "lblToanTu1";
+            lblToanTu1.Size = new Size(116, 109);
+            lblToanTu1.TabIndex = 2;
+            lblToanTu1.Text = "+";
+            lblToanTu1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblSo2
+            // 
+            lblSo2.BackColor = Color.FromArgb(255, 224, 192);
+            lblSo2.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSo2.Location = new Point(499, 152);
+            lblSo2.Name = "lblSo2";
+            lblSo2.Size = new Size(116, 109);
+            lblSo2.TabIndex = 3;
+            lblSo2.Text = "0";
+            lblSo2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblToanTu2
+            // 
+            lblToanTu2.BackColor = Color.FromArgb(255, 224, 192);
+            lblToanTu2.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblToanTu2.Location = new Point(688, 152);
+            lblToanTu2.Name = "lblToanTu2";
+            lblToanTu2.Size = new Size(116, 109);
+            lblToanTu2.TabIndex = 4;
+            lblToanTu2.Text = "-";
+            lblToanTu2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblSo3
+            // 
+            lblSo3.BackColor = Color.FromArgb(255, 224, 192);
+            lblSo3.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSo3.Location = new Point(874, 152);
+            lblSo3.Name = "lblSo3";
+            lblSo3.Size = new Size(116, 109);
+            lblSo3.TabIndex = 5;
+            lblSo3.Text = "0";
+            lblSo3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(57, 308);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 46);
+            label2.TabIndex = 6;
+            label2.Text = "Trả lời:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(57, 387);
+            label3.Name = "label3";
+            label3.Size = new Size(142, 46);
+            label3.TabIndex = 7;
+            label3.Text = "Kết quả:";
+            // 
+            // txtDoan
+            // 
+            txtDoan.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDoan.Location = new Point(222, 303);
+            txtDoan.Multiline = true;
+            txtDoan.Name = "txtDoan";
+            txtDoan.Size = new Size(597, 59);
+            txtDoan.TabIndex = 8;
+            // 
+            // lblKq
+            // 
+            lblKq.BackColor = SystemColors.ActiveCaption;
+            lblKq.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblKq.Location = new Point(222, 387);
+            lblKq.Name = "lblKq";
+            lblKq.Size = new Size(597, 57);
+            lblKq.TabIndex = 9;
+            lblKq.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnXem
+            // 
+            btnXem.BackColor = SystemColors.ActiveCaption;
+            btnXem.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnXem.Location = new Point(853, 303);
+            btnXem.Name = "btnXem";
+            btnXem.Size = new Size(159, 59);
+            btnXem.TabIndex = 10;
+            btnXem.Text = "Xem";
+            btnXem.UseVisualStyleBackColor = false;
+            btnXem.Click += btnXem_Click;
+            // 
+            // btnTiepTuc
+            // 
+            btnTiepTuc.BackColor = SystemColors.ActiveCaption;
+            btnTiepTuc.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTiepTuc.Location = new Point(853, 387);
+            btnTiepTuc.Name = "btnTiepTuc";
+            btnTiepTuc.Size = new Size(159, 59);
+            btnTiepTuc.TabIndex = 11;
+            btnTiepTuc.Text = "Tiếp tục";
+            btnTiepTuc.UseVisualStyleBackColor = false;
+            btnTiepTuc.Click += btnTiepTuc_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.BackColor = SystemColors.ActiveCaption;
+            btnXoa.BackgroundImage = (Image)resources.GetObject("btnXoa.BackgroundImage");
+            btnXoa.BackgroundImageLayout = ImageLayout.Stretch;
+            btnXoa.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnXoa.Location = new Point(652, 514);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(80, 52);
+            btnXoa.TabIndex = 31;
+            btnXoa.UseVisualStyleBackColor = false;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ActiveCaption;
+            button7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(577, 514);
+            button7.Name = "button7";
+            button7.Size = new Size(69, 52);
+            button7.TabIndex = 30;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += btn0_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveCaption;
+            button8.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(506, 514);
+            button8.Name = "button8";
+            button8.Size = new Size(69, 52);
+            button8.TabIndex = 29;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += btn0_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveCaption;
+            button9.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(443, 514);
+            button9.Name = "button9";
+            button9.Size = new Size(69, 52);
+            button9.TabIndex = 28;
+            button9.Text = "7";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += btn0_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaption;
+            button10.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.Location = new Point(376, 514);
+            button10.Name = "button10";
+            button10.Size = new Size(69, 52);
+            button10.TabIndex = 27;
+            button10.Text = "6";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += btn0_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.ActiveCaption;
+            button11.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.Location = new Point(307, 514);
+            button11.Name = "button11";
+            button11.Size = new Size(69, 52);
+            button11.TabIndex = 26;
+            button11.Text = "5";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += btn0_Click;
+            // 
+            // btnDauTru
+            // 
+            btnDauTru.BackColor = SystemColors.ActiveCaption;
+            btnDauTru.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDauTru.Location = new Point(652, 460);
+            btnDauTru.Name = "btnDauTru";
+            btnDauTru.Size = new Size(80, 52);
+            btnDauTru.TabIndex = 25;
+            btnDauTru.Text = "-";
+            btnDauTru.UseVisualStyleBackColor = false;
+            btnDauTru.Click += btnDauTru_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(577, 460);
+            button4.Name = "button4";
+            button4.Size = new Size(69, 52);
+            button4.TabIndex = 24;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += btn0_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(506, 460);
+            button3.Name = "button3";
+            button3.Size = new Size(69, 52);
+            button3.TabIndex = 23;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += btn0_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(443, 460);
+            button2.Name = "button2";
+            button2.Size = new Size(69, 52);
+            button2.TabIndex = 22;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += btn0_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(376, 460);
+            button1.Name = "button1";
+            button1.Size = new Size(69, 52);
+            button1.TabIndex = 21;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += btn0_Click;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = SystemColors.ActiveCaption;
+            btn0.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn0.Location = new Point(307, 460);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(69, 52);
+            btn0.TabIndex = 20;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            btn0.Click += btn0_Click;
+            // 
+            // PhepToan3So
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1103, 580);
+            Controls.Add(btnXoa);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(btnDauTru);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(btn0);
+            Controls.Add(btnTiepTuc);
+            Controls.Add(btnXem);
+            Controls.Add(lblKq);
+            Controls.Add(txtDoan);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(lblSo3);
+            Controls.Add(lblToanTu2);
+            Controls.Add(lblSo2);
+            Controls.Add(lblToanTu1);
+            Controls.Add(lblSo1);
+            Controls.Add(label1);
+            Name = "PhepToan3So";
+            Text = "PhepToan3So";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblSo1;
+        private Label lblToanTu1;
+        private Label lblSo2;
+        private Label lblToanTu2;
+        private Label lblSo3;
+        private Label label2;
+        private Label label3;
+        private TextBox txtDoan;
+        private Label lblKq;
+        private Button btnXem;
+        private Button btnTiepTuc;
+        private Button btnXoa;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button btnDauTru;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button btn0;
+    }
+}
---
+++ PhepToan3So.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormDangNhap
+{
+
+    public partial class PhepToan3So : Form
+    {
+        Random r = new Random();
+        string[] toantu1 = { "+", "-", "x", "/" };
+        string[] toantu2 = { "+", "-", "x", "/" };
+        int index1 = 0, index2 = 0;
+
+        public PhepToan3So()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTiepTuc_Click(object sender, EventArgs e)
+        {
+            int so1, so2, so3;
+            do
+            {
+                so1 = r.Next(10); so2 = r.Next(10); so3 = r.Next(10);
+                index1 = r.Next(4);
+                index2 = r.Next(4);
+
+            } while ((index1 == 3 && so2 == 0) || (index2 == 3 && so3 == 0));
+            lblSo1.Text = so1.ToString();
+            lblSo2.Text = so2.ToString();
+            lblSo3.Text = so3.ToString();
+            lblToanTu1.Text = toantu1[index1];
+            lblToanTu2.Text = toantu2[index2];
+            lblKq.Text = "";
+            txtDoan.Text = "";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            txtDoan.Text += bt.Text;
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            if ((index1 == 0 || index1 == 1) && (index2 == 2 || index2 == 3))
+            {
+                int Kq_23 = 0;
+                int Kq = 0;
+                switch (index2)
+                {
+                    case 2:
+                        Kq_23 = int.Parse(lblSo2.Text) * int.Parse(lblSo3.Text); break;
+                    case 3:
+                        Kq_23 = int.Parse(lblSo2.Text) / int.Parse(lblSo3.Text); break;
+                }
+
+                switch (index1)
+                {
+                    case 0:
+                        Kq = int.Parse(lblSo1.Text) + Kq_23; break;
+                    case 1:
+                        Kq = int.Parse(lblSo1.Text) - Kq_23; break;
+                }
+                try
+                {
+                    int user = int.Parse(txtDoan.Text);
+                    if (user == Kq)
+                    {
+                        lblKq.Text = "Chúc mừng bạn! Chính xác!!!";
+                    }
+                    else
+                    {
+                        lblKq.Text = "Tiếc quá! Sai rồi!!! Kết quả đúng phải là " + Kq.ToString();
+                    }
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Bạn phải nhập kết quả dự đoán là số!", "Thông báo");
+                }
+            }
+            else
+            {
+                int Kq_12 = 0;
+                int Kq = 0;
+                switch (index1)
+                {
+                    case 0:
+                        Kq_12 = int.Parse(lblSo1.Text) + int.Parse(lblSo2.Text); break;
+                    case 1:
+                        Kq_12 = int.Parse(lblSo1.Text) - int.Parse(lblSo2.Text); break;
+                    case 2:
+                        Kq_12 = int.Parse(lblSo1.Text) * int.Parse(lblSo2.Text); break;
+                    case 3:
+                        Kq_12 = int.Parse(lblSo1.Text) / int.Parse(lblSo2.Text); break;
+                }
+
+                switch (index2)
+                {
+                    case 0:
+                        Kq = Kq_12 + int.Parse(lblSo3.Text); break;
+                    case 1:
+                        Kq = Kq_12 - int.Parse(lblSo3.Text); break;
+                    case 2:
+                        Kq = Kq_12 * int.Parse(lblSo3.Text); break;
+                    case 3:
+                        Kq = Kq_12 / int.Parse(lblSo3.Text); break;
+                }
+                try
+                {
+                    int user = int.Parse(txtDoan.Text);
+                    if (user == Kq)
+                    {
+                        lblKq.Text = "Chúc mừng bạn! Chính xác!!!";
+                    }
+                    else
+                    {
+                        lblKq.Text = "Tiếc quá! Sai rồi!!! Kết quả đúng phải là " + Kq.ToString();
+                    }
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Bạn phải nhập kết quả dự đoán là số!", "Thông báo");
+                }
+            }
+
+        }
+
+        private void btnDauTru_Click(object sender, EventArgs e)
+        {
+            if (txtDoan.Text != "") return;
+            txtDoan.Text = "-";
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (txtDoan.Text == "") return;
+            txtDoan.Text = txtDoan.Text.Substring(0, txtDoan.Text.Length - 1);
+        }
+    }
+}
