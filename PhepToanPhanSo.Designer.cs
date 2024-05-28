50f93c2d47147167858b50ff4ee31e3022a83f22
matthew512a <kkien20030512@gmail.com>
2024-05-28 21:03:54 +0700

    Revert "chỉnh lại chữ ở giữa trong phân số"
    This reverts commit 0bc445cad3c956048e5c73f4ab898eb0dba64f18.

--- PhepToanPhanSo.Designer.cs
+++ PhepToanPhanSo.Designer.cs
@@ -56,9 +56,9 @@
             // 
             label1.BackColor = SystemColors.ActiveCaption;
             label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(3, 9);
+            label1.Location = new Point(-136, 9);
             label1.Name = "label1";
-            label1.Size = new Size(1012, 105);
+            label1.Size = new Size(1128, 79);
             label1.TabIndex = 1;
             label1.Text = "THỰC HIỆN PHÉP TOÁN VỚI PHÂN SỐ";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -69,31 +69,26 @@
             panel1.Controls.Add(Mau1);
             panel1.Controls.Add(Tu1);
             panel1.Controls.Add(textBox1);
-            panel1.Location = new Point(81, 148);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(71, 111);
             panel1.Name = "panel1";
-            panel1.Size = new Size(171, 219);
+            panel1.Size = new Size(150, 164);
             panel1.TabIndex = 36;
             // 
             // Mau1
             // 
             Mau1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Mau1.Location = new Point(30, 123);
-            Mau1.Margin = new Padding(3, 4, 3, 4);
+            Mau1.Location = new Point(26, 92);
             Mau1.Name = "Mau1";
-            Mau1.Size = new Size(114, 66);
+            Mau1.Size = new Size(100, 54);
             Mau1.TabIndex = 46;
-            Mau1.TextAlign = HorizontalAlignment.Center;
             // 
             // Tu1
             // 
             Tu1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Tu1.Location = new Point(30, 21);
-            Tu1.Margin = new Padding(3, 4, 3, 4);
+            Tu1.Location = new Point(26, 16);
             Tu1.Name = "Tu1";
-            Tu1.Size = new Size(114, 66);
+            Tu1.Size = new Size(100, 54);
             Tu1.TabIndex = 45;
-            Tu1.TextAlign = HorizontalAlignment.Center;
             // 
             // textBox1
             // 
@@ -103,11 +98,10 @@
             textBox1.BackColor = SystemColors.MenuHighlight;
             textBox1.BorderStyle = BorderStyle.None;
             textBox1.ForeColor = SystemColors.MenuHighlight;
-            textBox1.Location = new Point(30, 101);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(26, 76);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(114, 13);
+            textBox1.Size = new Size(100, 10);
             textBox1.TabIndex = 39;
             textBox1.UseWaitCursor = true;
             // 
@@ -116,31 +110,26 @@
             panel2.Controls.Add(Mau2);
             panel2.Controls.Add(Tu2);
             panel2.Controls.Add(textBox2);
-            panel2.Location = new Point(425, 148);
-            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Location = new Point(372, 111);
             panel2.Name = "panel2";
-            panel2.Size = new Size(171, 219);
+            panel2.Size = new Size(150, 164);
             panel2.TabIndex = 37;
             // 
             // Mau2
             // 
             Mau2.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Mau2.Location = new Point(29, 124);
-            Mau2.Margin = new Padding(3, 4, 3, 4);
+            Mau2.Location = new Point(25, 93);
             Mau2.Name = "Mau2";
-            Mau2.Size = new Size(114, 66);
+            Mau2.Size = new Size(100, 54);
             Mau2.TabIndex = 47;
-            Mau2.TextAlign = HorizontalAlignment.Center;
             // 
             // Tu2
             // 
             Tu2.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Tu2.Location = new Point(29, 21);
-            Tu2.Margin = new Padding(3, 4, 3, 4);
+            Tu2.Location = new Point(25, 16);
             Tu2.Name = "Tu2";
-            Tu2.Size = new Size(114, 66);
+            Tu2.Size = new Size(100, 54);
             Tu2.TabIndex = 46;
-            Tu2.TextAlign = HorizontalAlignment.Center;
             // 
             // textBox2
             // 
@@ -150,11 +139,10 @@
             textBox2.BackColor = SystemColors.MenuHighlight;
             textBox2.BorderStyle = BorderStyle.None;
             textBox2.ForeColor = SystemColors.MenuHighlight;
-            textBox2.Location = new Point(29, 103);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(25, 77);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(114, 13);
+            textBox2.Size = new Size(100, 10);
             textBox2.TabIndex = 40;
             textBox2.UseWaitCursor = true;
             // 
@@ -163,31 +151,26 @@
             panel3.Controls.Add(textBox9);
             panel3.Controls.Add(textBox8);
             panel3.Controls.Add(textBox3);
-            panel3.Location = new Point(765, 148);
-            panel3.Margin = new Padding(3, 4, 3, 4);
+            panel3.Location = new Point(669, 111);
             panel3.Name = "panel3";
-            panel3.Size = new Size(171, 219);
+            panel3.Size = new Size(150, 164);
             panel3.TabIndex = 38;
             // 
             // textBox9
             // 
             textBox9.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox9.Location = new Point(29, 123);
-            textBox9.Margin = new Padding(3, 4, 3, 4);
+            textBox9.Location = new Point(25, 92);
             textBox9.Name = "textBox9";
-            textBox9.Size = new Size(114, 66);
+            textBox9.Size = new Size(100, 54);
             textBox9.TabIndex = 47;
-            textBox9.TextAlign = HorizontalAlignment.Center;
             // 
             // textBox8
             // 
             textBox8.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox8.Location = new Point(29, 23);
-            textBox8.Margin = new Padding(3, 4, 3, 4);
+            textBox8.Location = new Point(25, 17);
             textBox8.Name = "textBox8";
-            textBox8.Size = new Size(114, 66);
+            textBox8.Size = new Size(100, 54);
             textBox8.TabIndex = 46;
-            textBox8.TextAlign = HorizontalAlignment.Center;
             // 
             // textBox3
             // 
@@ -197,11 +180,10 @@
             textBox3.BackColor = SystemColors.MenuHighlight;
             textBox3.BorderStyle = BorderStyle.None;
             textBox3.ForeColor = SystemColors.MenuHighlight;
-            textBox3.Location = new Point(29, 103);
-            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Location = new Point(25, 77);
             textBox3.Multiline = true;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(114, 13);
+            textBox3.Size = new Size(100, 10);
             textBox3.TabIndex = 40;
             textBox3.UseWaitCursor = true;
             // 
@@ -211,9 +193,9 @@
             Dau.BackColor = Color.Transparent;
             Dau.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Dau.ForeColor = SystemColors.Highlight;
-            Dau.Location = new Point(315, 208);
+            Dau.Location = new Point(276, 156);
             Dau.Name = "Dau";
-            Dau.Size = new Size(76, 81);
+            Dau.Size = new Size(61, 65);
             Dau.TabIndex = 30;
             Dau.Text = "+";
             Dau.TextAlign = ContentAlignment.MiddleCenter;
@@ -224,9 +206,9 @@
             label10.BackColor = Color.Transparent;
             label10.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label10.ForeColor = SystemColors.Highlight;
-            label10.Location = new Point(664, 208);
+            label10.Location = new Point(581, 156);
             label10.Name = "label10";
-            label10.Size = new Size(76, 81);
+            label10.Size = new Size(61, 65);
             label10.TabIndex = 40;
             label10.Text = "=";
             label10.TextAlign = ContentAlignment.MiddleCenter;
@@ -234,10 +216,9 @@
             // btnCong
             // 
             btnCong.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnCong.Location = new Point(220, 468);
-            btnCong.Margin = new Padding(3, 4, 3, 4);
+            btnCong.Location = new Point(204, 351);
             btnCong.Name = "btnCong";
-            btnCong.Size = new Size(78, 79);
+            btnCong.Size = new Size(57, 59);
             btnCong.TabIndex = 41;
             btnCong.Text = "+";
             btnCong.TextAlign = ContentAlignment.TopCenter;
@@ -247,10 +228,9 @@
             // btnTru
             // 
             btnTru.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnTru.Location = new Point(349, 468);
-            btnTru.Margin = new Padding(3, 4, 3, 4);
+            btnTru.Location = new Point(325, 351);
             btnTru.Name = "btnTru";
-            btnTru.Size = new Size(87, 79);
+            btnTru.Size = new Size(57, 59);
             btnTru.TabIndex = 42;
             btnTru.Text = "-";
             btnTru.TextAlign = ContentAlignment.TopCenter;
@@ -260,10 +240,9 @@
             // btnNhan
             // 
             btnNhan.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnNhan.Location = new Point(483, 468);
-            btnNhan.Margin = new Padding(3, 4, 3, 4);
+            btnNhan.Location = new Point(440, 351);
             btnNhan.Name = "btnNhan";
-            btnNhan.Size = new Size(85, 79);
+            btnNhan.Size = new Size(57, 59);
             btnNhan.TabIndex = 43;
             btnNhan.Text = "x";
             btnNhan.TextAlign = ContentAlignment.TopCenter;
@@ -273,10 +252,9 @@
             // btnChia
             // 
             btnChia.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnChia.Location = new Point(610, 468);
-            btnChia.Margin = new Padding(3, 4, 3, 4);
+            btnChia.Location = new Point(552, 351);
             btnChia.Name = "btnChia";
-            btnChia.Size = new Size(86, 79);
+            btnChia.Size = new Size(57, 59);
             btnChia.TabIndex = 44;
             btnChia.Text = "/";
             btnChia.TextAlign = ContentAlignment.TopCenter;
@@ -285,9 +263,9 @@
             // 
             // PhepToanPhanSo
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1016, 691);
+            ClientSize = new Size(889, 518);
             Controls.Add(btnChia);
             Controls.Add(btnNhan);
             Controls.Add(btnTru);
@@ -298,7 +276,6 @@
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "PhepToanPhanSo";
             Text = "PhepToanPhanSo";
             panel1.ResumeLayout(false);
